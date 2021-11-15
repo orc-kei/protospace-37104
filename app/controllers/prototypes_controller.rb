736326f19cd598169d475cbf6d8d2c87849d1dce
orc-kei <keeeei6228@gmail.com>
2021-11-15 11:54:07 +0900

    Update prototypes_controller.rb

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,7 @@
 class PrototypesController < ApplicationController
   before_action :set_prototype, except: [:index, :new, :create]
   before_action :authenticate_user! , except: [:index, :show]
-  before_action :move_to_index, only: [:new, :edit, :destroy]
+  before_action :move_to_index, only: [:update, :edit, :destroy]
 
 
   def index
