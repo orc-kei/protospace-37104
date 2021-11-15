82d431e6f7c0264ea769fa40c8d8b153bcd441db
orc-kei <keeeei6228@gmail.com>
2021-11-15 11:20:58 +0900

    Update prototypes_controller.rb

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,5 @@
 class PrototypesController < ApplicationController
+  before_action :set_prototype, except: [:index, :new, :create]
   before_action :authenticate_user! , except: [:index, :show]
   before_action :move_to_index, only: [:new, :edit, :destroy]
 
@@ -60,10 +61,12 @@ end
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
+
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
   
   def move_to_index
-    unless user_signed_in?
-    redirect_to action: :index
-    end
+    redirect_to root_path unless current_user == @prototype.user
   end
 end
